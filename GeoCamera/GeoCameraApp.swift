6b30a8ad745c060e491c2537ec3a4547c0e80cde
cheebolino <106555831+cheebolino@users.noreply.github.com>
2022-10-12 17:28:00 +0300

    Initial Commit

---
+++ GeoCamera/GeoCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCameraApp.swift
+//  GeoCamera
+//
+//  Created by Cheeba on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
