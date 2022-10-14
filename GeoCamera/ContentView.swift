48b96520ab64e77b8cd338a36c120fb368bf8bca
cheebolino <106555831+cheebolino@users.noreply.github.com>
2022-10-14 14:14:28 +0300

    Added LocationManager
    Display User coordinates, altitude and orientation

--- GeoCamera/ContentView.swift
+++ GeoCamera/ContentView.swift
@@ -7,17 +7,41 @@
 
 import SwiftUI
 import AVFoundation
+import CoreLocation
 
 struct ContentView: View {
     
     @StateObject var camera = CameraModel()
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    var userAltitude: String {
+        return "\(locationManager.lastLocation?.altitude ?? 0)"
+    }
+    
+    var userHeading: String {
+        return "\(locationManager.lastHeading?.trueHeading ?? 0)"
+    }
     
     var body: some View {
         ZStack {
             CameraPreview(camera: camera)
                 .ignoresSafeArea()
             
-            Text("henlo")
+            VStack {
+                Text("\(userLatitude)")
+                Text("\(userLongitude)")
+                Text("\(userAltitude)")
+                Text("\(userHeading)")
+            }
+            
         }
         .onAppear() {
             camera.Check()
@@ -103,4 +127,32 @@ struct CameraPreview: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) { }
+}
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    private let locationManager = CLLocationManager()
+    @Published var locationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+    @Published var lastHeading: CLHeading?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastLocation = locations.first
+        print(#function, locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        lastHeading = newHeading
+        print(#function, newHeading)
+    }
+    
 }
