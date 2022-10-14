561f6fc8760bae30da42cdcc6a71de026b471560
cheebolino <106555831+cheebolino@users.noreply.github.com>
2022-10-14 19:19:47 +0300

    Update ContentView.swift
    Added compass and device pitch to viewfinder

--- GeoCamera/ContentView.swift
+++ GeoCamera/ContentView.swift
@@ -8,26 +8,41 @@
 import SwiftUI
 import AVFoundation
 import CoreLocation
+import CoreMotion
 
 struct ContentView: View {
     
     @StateObject var camera = CameraModel()
     @StateObject var locationManager = LocationManager()
+    @StateObject var motionManager = MotionManager()
     
     var userLatitude: String {
-        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+        
+        return String(format: "%.3f", locationManager.lastLocation?.coordinate.latitude ?? 0)
     }
     
     var userLongitude: String {
-        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+        return String(format: "%.3f", locationManager.lastLocation?.coordinate.longitude ?? 0)
+        
     }
     
     var userAltitude: String {
-        return "\(locationManager.lastLocation?.altitude ?? 0)"
+        return String(format: "%.3f", locationManager.lastLocation?.altitude ?? 0)
+//        return "\(locationManager.lastLocation?.altitude ?? 0)"
     }
     
     var userHeading: String {
-        return "\(locationManager.lastHeading?.trueHeading ?? 0)"
+        return String(format: "%.1f", locationManager.lastHeading?.trueHeading ?? 0)
+//        return "\(locationManager.lastHeading?.trueHeading ?? 0)"
+    }
+    
+    var userPitch: String {
+        return String(format: "%.1f", motionManager.pitch ?? 0)
+//        return "\(motionManager.pitch)"
+    }
+    
+    var userRoll: String {
+        return "\(motionManager.roll)"
     }
     
     var body: some View {
@@ -35,17 +50,37 @@ struct ContentView: View {
             CameraPreview(camera: camera)
                 .ignoresSafeArea()
             
+            Circle().frame(width: 10, height: 10)
+            
             VStack {
-                Text("\(userLatitude)")
-                Text("\(userLongitude)")
-                Text("\(userAltitude)")
-                Text("\(userHeading)")
+                HStack {
+                    VStack {
+                        Text(userLatitude)
+                        Text(userLongitude)
+                    }
+                    Spacer()
+                    Text(userAltitude)
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text(userHeading)
+                    Spacer()
+                    Text(userPitch)
+                    //Text(userRoll)
+                }
+                
             }
             
         }
         .onAppear() {
             camera.Check()
         }
+    }
+    
+    func round3digits(number: Float?) -> Float {
+        return round((number ?? 0) * 1000) / 1000.0
     }
 }
 
@@ -118,7 +153,7 @@ struct CameraPreview: UIViewRepresentable {
         
         camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
         camera.preview.frame = view.frame
-        camera.preview.videoGravity = .resize
+        camera.preview.videoGravity = .resizeAspectFill
         
         view.layer.addSublayer(camera.preview)
         camera.session.startRunning()
@@ -154,5 +189,34 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         lastHeading = newHeading
         print(#function, newHeading)
     }
+    
+}
+
+class MotionManager: NSObject, ObservableObject {
+    
+    private let motionManager = CMMotionManager()
+    @Published var pitch = 0.0
+    @Published var roll = 0.0
+    
+    override init() {
+        super.init()
+        
+        motionManager.deviceMotionUpdateInterval = 1/60
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: .main) { (motionData, error) in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                
+                if let motionData = motionData {
+                    self.pitch = motionData.attitude.pitch * 57.3
+                    self.roll  = motionData.attitude.roll
+                }
+            }
+        }
+    }
+    
     
 }
