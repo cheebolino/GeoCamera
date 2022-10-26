9cc3e040c09e28e8e9d605fd5ac571537ba96051
cheebolino <106555831+cheebolino@users.noreply.github.com>
2022-10-26 13:26:35 +0300

    Refactoring Camera

--- GeoCamera/ContentView.swift
+++ GeoCamera/ContentView.swift
@@ -6,9 +6,6 @@
 //
 
 import SwiftUI
-import AVFoundation
-import CoreLocation
-import CoreMotion
 
 struct ContentView: View {
     
@@ -17,13 +14,11 @@ struct ContentView: View {
     @StateObject var motionManager = MotionManager()
     
     var userLatitude: String {
-        
         return String(format: "%.3f", locationManager.lastLocation?.coordinate.latitude ?? 0)
     }
     
     var userLongitude: String {
         return String(format: "%.3f", locationManager.lastLocation?.coordinate.longitude ?? 0)
-        
     }
     
     var userAltitude: String {
@@ -82,7 +77,7 @@ struct ContentView: View {
                     //Text(userRoll)
                 }
                 
-            }
+            }.padding(20)
             
         }
         .onAppear() {
@@ -100,78 +95,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-class CameraModel: ObservableObject {
-    
-    @Published var isTaken = false
-    @Published var session = AVCaptureSession()
-    @Published var alert = false
-    @Published var output = AVCapturePhotoOutput()
-    @Published var preview: AVCaptureVideoPreviewLayer!
-    
-    func Check() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-            
-        case .authorized:
-            SetUp()
-            return
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { status in
-                if status {
-                    self.SetUp()
-                }
-            }
-        case .denied:
-            self.alert.toggle()
-            return
-        case .restricted:
-            return
-        @unknown default:
-            return
-        }
-    }
-    
-    func SetUp() {
-        //setting up camera
-        
-        do {
-            self.session.beginConfiguration()
-            
-            let device = AVCaptureDevice.default(for: .video)
-            let input = try AVCaptureDeviceInput(device: device!)
-            
-            if self.session.canAddInput(input) {
-                self.session.addInput(input)
-            }
-            
-            if self.session.canAddOutput(self.output) {
-                self.session.addOutput(self.output)
-            }
-            
-            self.session.commitConfiguration()
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-}
-
-struct CameraPreview: UIViewRepresentable {
-    
-    @ObservedObject var camera: CameraModel
-    
-    func makeUIView(context: Context) -> some UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
-        
-        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
-        camera.preview.frame = view.frame
-        camera.preview.videoGravity = .resizeAspectFill
-        
-        view.layer.addSublayer(camera.preview)
-        camera.session.startRunning()
-        
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) { }
-}
-
